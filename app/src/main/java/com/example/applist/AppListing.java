5ab507f1fe250306ec932f10906b9b1ab18adf70
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-21 13:23:51 -0700

    Added husk of Data Model and Repo Class

---
+++ app/src/main/java/com/example/applist/AppListing.java
@@ -0,0 +1,50 @@
+package com.example.applist;
+
+public class AppListing {
+    //id,app_name,app_version,domain_name,contact_email,image_url
+    private int listingId =0;
+    private String appVersion;
+    private String domainName= "";
+    private String contactEmail= "";
+    private String imageURL="";
+
+    public int getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(int listingId) {
+        this.listingId = listingId;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
---
+++ app/src/main/java/com/example/applist/AppRepository.java
@@ -0,0 +1,8 @@
+package com.example.applist;
+
+public class AppRepository {
+    private final String APP_DATA = "";
+
+   
+}
+
