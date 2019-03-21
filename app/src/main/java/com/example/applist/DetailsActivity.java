43efc6aae2cf1274df57d514db2c44b8082d4e2b
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-21 16:11:52 -0700

    Added activity detail variable definitions

--- app/src/main/java/com/example/applist/DetailsActivity.java
+++ app/src/main/java/com/example/applist/DetailsActivity.java
@@ -1,7 +1,23 @@
 package com.example.applist;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
-private AppListing app;
+    private AppListing app;
+    private TextView nameView;
+    private TextView vView;
+    private TextView domainView;
+    private TextView emailView;
+    private ImageView imageView;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    
+
+    }
 }
