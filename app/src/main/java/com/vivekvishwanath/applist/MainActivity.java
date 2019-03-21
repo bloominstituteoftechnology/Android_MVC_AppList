be83a1b4f594dcef52f7f795134921ba1a26005a
VivekV95 <vivekv210@gmail.com>
2019-03-21 16:43:13 -0500

    Created TextView generator to list applications by name

--- app/src/main/java/com/vivekvishwanath/applist/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/applist/MainActivity.java
@@ -1,15 +1,33 @@
 package com.vivekvishwanath.applist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    LinearLayout appListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        ArrayList<AppListing> appListings = AppRepository.getAppListings();
 
-        AppRepository.getAppListings();
+        appListView = findViewById(R.id.app_list_view);
+        
+    }
+
+    public TextView createAppView(AppListing appListing) {
+        TextView appView = new TextView(context);
+        appView.setTextSize(20);
+        appView.setPadding(5,5,5,5);
+        appView.setText("Application Name : " + appListing.getAppName());
+        return appView;
     }
 }
