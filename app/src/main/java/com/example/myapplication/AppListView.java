ef0d33a7f8991ea6d0ce70449f04cc64ef58983a
BL-CoderOvertime <brandonlively@live.com>
2019-03-21 15:52:36 -0400

    Added AppRepo and AppListing Classes Added APP_DATA variable

--- app/src/main/java/com/example/myapplication/AppListView.java
+++ app/src/main/java/com/example/myapplication/AppListView.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AppListView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/myapplication/AppListing.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+public class AppListing {
+
+    //id,app_name,app_version,domain_name,contact_email,image_url
+
+    private int id;
+    private String appName;
+    private String appVersion;
+    private String domainName;
+    private String contactEmail;
+    private String imageURL;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
