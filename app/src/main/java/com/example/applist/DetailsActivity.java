d33b6c6647eadd4feb2fa3ac9c523cf60444fca4
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-21 15:35:07 -0700

    Added Details Activity

---
+++ app/src/main/java/com/example/applist/DetailsActivity.java
@@ -0,0 +1,5 @@
+package com.example.applist;
+
+public class DetailsActivity {
+    
+}
--- app/src/main/java/com/example/applist/MainActivity.java
+++ app/src/main/java/com/example/applist/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.applist;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+    }
+
+    private TextView createAppView(final AppListing appView) {
+        TextView view = new TextView(context);
+        view.setText(appView.getAppName());
+        view.setPadding(15, 15, 15, 15);
+        view.setTextSize(22);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewImageIntent = new Intent(context, );
+            }
+        });
+
+
+
     }
 }
