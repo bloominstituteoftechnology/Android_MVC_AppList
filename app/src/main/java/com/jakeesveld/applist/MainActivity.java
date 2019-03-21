0cd9d811a25536b9900f41a45959a05911c30d40
Jake Esveld <thejakeesveld@gmail.com>
2019-03-21 13:38:39 -0700

    Add AppListing model and AppRepository class

--- app/src/main/java/com/jakeesveld/applist/MainActivity.java
+++ app/src/main/java/com/jakeesveld/applist/MainActivity.java
@@ -1,13 +1,19 @@
 package com.jakeesveld.applist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
     }
+
 }
