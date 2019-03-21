6482bd58171967f984ae1a136a3cc281de873d16
Spencer Stock <spencerstock@gmail.com>
2019-03-21 16:36:57 -0600

    non functional version, trying to resolve putExtra error

--- app/src/main/java/com/example/applist/MainActivity.java
+++ app/src/main/java/com/example/applist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.applist;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,11 +25,25 @@ public class MainActivity extends AppCompatActivity {
         appListings = getAppListings();
 
 
-        for (AppListing listing : appListings){
+        for (final AppListing listing : appListings){
             TextView tv = new TextView(this);
             LinearLayout linearLayout = findViewById(R.id.linear_layout_child);
             tv.setText(listing.getApp_name());
             tv.setTextSize(25);
+            tv.setTypeface(Typeface.DEFAULT_BOLD);
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AppListing temp = listing;
+                    Intent intent = new Intent(getApplicationContext(),DetailView.class);
+                    intent.putExtra("LISTING_KEY", temp);
+                    startActivity(intent);
+
+
+                }
+            });
 
 
 
