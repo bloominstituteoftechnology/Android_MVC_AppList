65bd899575fb3b7a4e79175ce0db6dbf346ec8e5
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-21 15:44:09 -0700

    Added activity_details XML

--- app/src/main/java/com/example/applist/DetailsActivity.java
+++ app/src/main/java/com/example/applist/DetailsActivity.java
@@ -1,5 +1,7 @@
 package com.example.applist;
+import android.support.v7.app.AppCompatActivity;
 
-public class DetailsActivity {
-    
+public class DetailsActivity extends AppCompatActivity {
+private AppListing app;
+
 }
