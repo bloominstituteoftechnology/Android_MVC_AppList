da14975bb81b1afb266260667cdbe88111ead2a1
voodoopdx <ourquotesdefineus@gmail.com>
2019-03-21 18:24:47 -0700

    finished activity detail view and completed app v0.1.001

--- app/src/main/java/com/example/applist/MainActivity.java
+++ app/src/main/java/com/example/applist/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = this;
+
+        LinearLayout l = (LinearLayout) findViewById(R.id.line_view);
+
+        ArrayList<AppListing> apps = AppRepository.getAppListings();
+
+        for(AppListing a:apps) {
+            TextView t = createAppView(a);
+            l.addView(t);
+        }
     }
 
     private TextView createAppView(final AppListing appView) {
