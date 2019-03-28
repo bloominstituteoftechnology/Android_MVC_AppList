a71e94b2d5dc87ef2ea6925b66293cb687a95d29
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-28 16:26:12 -0500

    Create Appdetails activity

---
+++ app/src/main/java/com/example/applist/AppDetailView.java
@@ -0,0 +1,13 @@
+package com.example.applist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_detail_view);
+    }
+}
