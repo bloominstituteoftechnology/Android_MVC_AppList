52794e97485fa2c18299c2bd6df003ee481a9bdf
AlhousseineC <Alhoussein.camara@gmail.com>
2019-03-28 12:53:41 -0500

    Alhousseine Camara - App List

---
+++ app/src/main/java/com/example/applist/AppListModel.java
@@ -0,0 +1,68 @@
+package com.example.applist;
+
+public class AppListModel {
+
+    private int id;
+    private String appName,appVersion,domainName,contactEmail,imageUrl;
+
+    public AppListModel(int id, String appName, String appVersion, String domainName, String contactEmail, String imageUrl) {
+        this.id = id;
+        this.appName = appName;
+        this.appVersion = appVersion;
+        this.domainName = domainName;
+        this.contactEmail = contactEmail;
+        this.imageUrl = imageUrl;
+    }
+
+    public AppListModel(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
