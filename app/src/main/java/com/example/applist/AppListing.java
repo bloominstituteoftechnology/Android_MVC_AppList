113f69ab458cfb7c33fd0e3ee1ab6e6115442fc1
Janiece Thomas <janiece.t90@gmail.com>
2019-03-28 18:01:19 -0400

    Created private data members along with getters and setters

---
+++ app/src/main/java/com/example/applist/AppListing.java
@@ -0,0 +1,69 @@
+package com.example.applist;
+
+import android.net.Uri;
+
+public class AppListing {
+
+     //id,app_name,app_version,domain_name,contact_email,image_url
+
+    private int id, app_version;
+    private String app_name, domain_name, contact_email;
+    private Uri image_url;
+
+    public AppListing(int id, int app_version, String app_name, String domain_name, String contact_email, Uri image_url) {
+        this.id = id;
+        this.app_version = app_version;
+        this.app_name = app_name;
+        this.domain_name = domain_name;
+        this.contact_email = contact_email;
+        this.image_url = image_url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getApp_version() {
+        return app_version;
+    }
+
+    public void setApp_version(int app_version) {
+        this.app_version = app_version;
+    }
+
+    public String getApp_name() {
+        return app_name;
+    }
+
+    public void setApp_name(String app_name) {
+        this.app_name = app_name;
+    }
+
+    public String getDomain_name() {
+        return domain_name;
+    }
+
+    public void setDomain_name(String domain_name) {
+        this.domain_name = domain_name;
+    }
+
+    public String getContact_email() {
+        return contact_email;
+    }
+
+    public void setContact_email(String contact_email) {
+        this.contact_email = contact_email;
+    }
+
+    public Uri getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(Uri image_url) {
+        this.image_url = image_url;
+    }
+}
--- app/src/main/java/com/example/applist/DataModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.applist;
-
-public class DataModel {
-}
